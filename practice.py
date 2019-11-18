aa5164ecf1f064578b15e069b949cad9534b6bee
KHO11 <54800683+KHO11@users.noreply.github.com>
2019-11-18 11:04:33 +0000

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,19 @@
+from math import *
+print("hello world")
+
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+character = "John"
+age = 50
+print("hello my name is " + character)
+print("giraffe\nacademy")
+print(sqrt(100))
+
+
+
+
+
+
